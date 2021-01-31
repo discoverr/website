e6a7bee45925a26affabd43096efa61eea6f969c
Thupi Ceylons <thupi@ceylons.net>
2021-01-31 17:52:51 +0000

    Remove safari fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,6 @@ import React from "react";
 import useMedia from "react-use/lib/useMedia";
 import { AnimatePresence, AnimateSharedLayout, motion } from "framer-motion";
 import styled from "styled-components";
-import FullScreen from "mobile-safari-fullscreen";
-import "mobile-safari-fullscreen/dist/index.module.css";
 
 import { hoverProps } from "../components/Hoverable";
 
@@ -201,134 +199,133 @@ export default () => {
 
       <AnimatePresence>
         {selectedItem && (
-          <FullScreen classNames={{ iosFix: "iosFix" }} isOpen={true}>
+          <motion.div
+            style={{
+              position: "fixed",
+              top: 0,
+              right: 0,
+              left: 0,
+              bottom: 0,
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
             <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
               style={{
-                position: "fixed",
+                position: "absolute",
                 top: 0,
                 right: 0,
-                left: 0,
                 bottom: 0,
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "center",
-                alignItems: "center",
+                left: 0,
+                backgroundColor: "#fff",
               }}
             >
+              <button
+                style={{
+                  position: "absolute",
+                  zIndex: 10,
+                  top: 30,
+                  right: 30,
+                  padding: 0,
+                  margin: 0,
+                  border: "none",
+                  outline: "none",
+                  backgroundColor: "transparent",
+                }}
+                onClick={() => setSelectedItem(null)}
+                {...hoverProps}
+              >
+                <X size={40} />
+              </button>
+            </motion.div>
+            <RemoveScroll>
               <motion.div
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
                 style={{
-                  position: "absolute",
-                  top: 0,
-                  right: 0,
-                  bottom: 0,
-                  left: 0,
-                  backgroundColor: "#fff",
+                  maxWidth: 1100,
+                  position: "relative",
+                  zIndex: 2,
                 }}
               >
-                <button
+                <div
                   style={{
-                    position: "absolute",
-                    top: 30,
-                    right: 30,
-                    padding: 0,
-                    margin: 0,
-                    border: "none",
-                    outline: "none",
-                    backgroundColor: "transparent",
-                  }}
-                  onClick={() => setSelectedItem(null)}
-                  {...hoverProps}
-                >
-                  <X size={40} />
-                </button>
-              </motion.div>
-              <RemoveScroll>
-                <motion.div
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  style={{
-                    maxWidth: 1100,
-                    position: "relative",
-                    zIndex: 2,
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
                   }}
                 >
+                  <motion.div
+                    layoutId={selectedItem.id}
+                    style={{
+                      position: "relative",
+                      width: "329px",
+                      minWidth: "329px",
+                      height: "329px",
+                      backgroundColor: selectedItem.backgroundColor,
+                      // backgroundImage:
+                      //   "url('https://i.pinimg.com/originals/fc/68/f8/fc68f86873c9c661e84ad442cf8fb6cf.gif')",
+                      backgroundSize: "cover",
+                    }}
+                    {...hoverProps}
+                  ></motion.div>
                   <div
                     style={{
-                      display: "flex",
-                      flexDirection: "row",
-                      alignItems: "center",
+                      paddingLeft: 90,
                     }}
                   >
-                    <motion.div
-                      layoutId={selectedItem.id}
+                    <h3
                       style={{
-                        position: "relative",
-                        width: "329px",
-                        minWidth: "329px",
-                        height: "329px",
-                        backgroundColor: selectedItem.backgroundColor,
-                        // backgroundImage:
-                        //   "url('https://i.pinimg.com/originals/fc/68/f8/fc68f86873c9c661e84ad442cf8fb6cf.gif')",
-                        backgroundSize: "cover",
-                      }}
-                      {...hoverProps}
-                    ></motion.div>
-                    <div
-                      style={{
-                        paddingLeft: 90,
+                        margin: 0,
+                        padding: 0,
+                        paddingBottom: 60,
+                        flexGrow: 1,
+                        fontSize: 42,
+                        fontFamily: "Montserrat",
+                        fontWeight: 700,
                       }}
                     >
-                      <h3
+                      The public is more familiar with bad design than good
+                      design.
+                    </h3>
+                    <p
+                      style={{
+                        position: "relative",
+                        fontSize: "16px",
+                        fontFamily: "Montserrat",
+                        lineHeight: 1.8,
+                        margin: 0,
+                        padding: 0,
+                        paddingLeft: 100,
+                        color: "#444",
+                      }}
+                    >
+                      <span
                         style={{
-                          margin: 0,
-                          padding: 0,
-                          paddingBottom: 60,
-                          flexGrow: 1,
-                          fontSize: 42,
-                          fontFamily: "Montserrat",
-                          fontWeight: 700,
+                          position: "absolute",
+                          top: 0,
+                          left: 0,
+                          width: 2,
+                          height: 60,
+                          backgroundColor: "#000",
                         }}
-                      >
-                        The public is more familiar with bad design than good
-                        design.
-                      </h3>
-                      <p
-                        style={{
-                          position: "relative",
-                          fontSize: "16px",
-                          fontFamily: "Montserrat",
-                          lineHeight: 1.8,
-                          margin: 0,
-                          padding: 0,
-                          paddingLeft: 100,
-                          color: "#444",
-                        }}
-                      >
-                        <span
-                          style={{
-                            position: "absolute",
-                            top: 0,
-                            left: 0,
-                            width: 2,
-                            height: 60,
-                            backgroundColor: "#000",
-                          }}
-                        ></span>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Vivamus non vulputate ipsum, id dignissim ante. Cras
-                        posuere eros justo, pulvinar dignissim neque cursus non.
-                        Ut sed purus vestibulum lacus tristique bibendum.
-                      </p>
-                    </div>
+                      ></span>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Vivamus non vulputate ipsum, id dignissim ante. Cras
+                      posuere eros justo, pulvinar dignissim neque cursus non.
+                      Ut sed purus vestibulum lacus tristique bibendum.
+                    </p>
                   </div>
-                </motion.div>
-              </RemoveScroll>
-            </motion.div>
-          </FullScreen>
+                </div>
+              </motion.div>
+            </RemoveScroll>
+          </motion.div>
         )}
       </AnimatePresence>
     </AnimateSharedLayout>
